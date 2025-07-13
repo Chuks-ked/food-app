e80027d32ba7f07fd0b5353823b7f2c71c1592b3
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-07-13 07:21:02 +0100

    dns added to env

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,26 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
+import * as Sentry from '@sentry/react-native';
 import './globals.css';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
     'Quicksand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -19,4 +36,4 @@ export default function RootLayout() {
   }, [fontsLoaded, error])
 
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+});

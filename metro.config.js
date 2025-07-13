563cc797deaa03688f6b0c3cbf713ffa66fa03a5
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-07-13 07:13:54 +0100

    sentry  config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,14 @@
-const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getSentryExpoConfig(__dirname)
 
 module.exports = withNativeWind(config, { input: './app/globals.css' })
+
+// use this conf if sentry is uninstalled
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require('nativewind/metro');
+// const config = getDefaultConfig(__dirname)
+// module.exports = withNativeWind(config, { input: './app/globals.css' })
